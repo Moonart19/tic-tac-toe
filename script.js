6a892edd063b6421881775c55d608c5da140bdcc
Moonart19 <mm4u005@gmail.com>
2025-07-07 10:07:01 +0530

    Fixed bugs and added new font

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
+const playerOneElement = document.querySelector("#player1");
+const playerTwoElement = document.querySelector("#player2");
+const message = document.querySelector(".js-message");
+const playerGameMessage = document.querySelector(".js-player-name-message");
+
+function playerMessageDisplayBlock() {
+  playerGameMessage.style.display = "block";
+}
+
+function playerMessageDisplayNone() {
+  playerGameMessage.style.display = "none";
+}
+
 const Gameboard = (() => {
   let gameboard = ["", "", "", "", "", "", "", "", ""];
+
+  const clear = () => {
+    const container = document.querySelector(".gameboard");
+    container.innerHTML = "";
+  };
 
   const render = () => {
     gameboard.forEach((cell, index) => {
       createCell(cell, index);
     });
-  };
-
-  const clear = () => {
-    const container = document.querySelector(".gameboard");
-    container.innerHTML = "";
   };
 
   return {
@@ -40,8 +53,7 @@ const Game = (() => {
   ];
 
   const checkWinner = (winner, marker) => {
-    const message = document.querySelector(".js-message");
-    const playerGameMessage = document.querySelector(".js-player-name-message");
+    let isWin = false;
     let isFull = Gameboard.gameboard.every(
       (cell) => cell !== "" && cell !== null && cell !== undefined
     );
@@ -53,30 +65,34 @@ const Game = (() => {
         Gameboard.gameboard[b] === marker &&
         Gameboard.gameboard[c] === marker
       ) {
-        gameOver = true;
-        message.textContent = `${winner} won the game`;
-        playerGameMessage.textContent = `Enter the players name before restarting`;
-        restart.setRestart();
-      } else if (isFull) {
-        gameOver = true;
-        message.textContent = `It's a tie`;
-        playerGameMessage.textContent = `Enter the players name before restarting`;
-        restart.setRestart();
+        isWin = true;
       }
     });
+
+    if (isWin) {
+      gameOver = true;
+      players = [];
+      message.innerHTML = `<span class="player-winner">${winner}</span> won the game`;
+      playerMessageDisplayBlock();
+      restart.setRestart();
+    } else if (isFull) {
+      gameOver = true;
+      message.textContent = `It's a tie`;
+      playerMessageDisplayBlock();
+      restart.setRestart();
+    }
   };
 
   const start = () => {
     const startBtn = document.querySelector(".js-start-button");
     startBtn.textContent = "Start";
-    // const message = document.querySelector(".js-message");
+
+    playerMessageDisplayNone();
 
     gameOver = false;
-    const playerOneElement = document.querySelector("#player1");
     const player1 = Player(playerOneElement.value, "X");
     playerOneElement.value = "";
 
-    const playerTwoElement = document.querySelector("#player2");
     const player2 = Player(playerTwoElement.value, "O");
     players = [player1, player2];
     playerTwoElement.value = "";
@@ -91,7 +107,7 @@ const Game = (() => {
     cells.forEach((cell, index) => {
       cell.addEventListener("click", () => {
         if (!gameOver && cell && Gameboard.gameboard[index] === "") {
-          playerTurnName.textContent = `${currentPlayer.name} has marked`;
+          playerTurnName.innerHTML = `<span class="player-playing">${currentPlayer.name}</span> has marked`;
           Gameboard.gameboard[index] = currentPlayer.marker;
           cell.textContent = Gameboard.gameboard[index];
           checkWinner(currentPlayer.name, currentPlayer.marker);
@@ -111,19 +127,16 @@ const Game = (() => {
 
 const restart = (() => {
   const restartBtn = document.querySelector(".js-start-button");
+
   const clearValues = () => {
     Gameboard.gameboard = ["", "", "", "", "", "", "", "", ""];
-    Game.players = [];
-  };
-  const setRestart = () => {
-    restartBtn.textContent = "Restart";
-    clearValues();
-    restartBtn.addEventListener("click", () => {
-      Game.start();
-    });
   };
 
-  return { setRestart };
+  const setRestart = () => {
+    restartBtn.textContent = "Restart";
+  };
+
+  return { setRestart, clearValues };
 })();
 
 function createCell(index) {
@@ -136,5 +149,7 @@ function createCell(index) {
 
 const startButton = document.querySelector(".js-start-button");
 startButton.addEventListener("click", () => {
+  restart.clearValues();
+  message.textContent = "Enter the players name";
   Game.start();
 });
